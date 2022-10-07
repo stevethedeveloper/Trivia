f73e77ae38015e32864464afab9e0b3e89803aaf
Stephen Walton <info@stevethedeveloper.com>
2022-10-07 13:00:50 -0700

    Initial Commit

---
+++ Trivia/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Trivia
+//
+//  Created by Stephen Walton on 10/7/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
